f238c5e9a2e8fb05248c1d3ff9318ea08362c457
Ricardo Apú Chinchilla <ricardoapuch@gmail.com>
2019-08-07 12:29:12 -0600

    images and chart files

--- naive_bayes.r
+++ naive_bayes.r
@@ -2,23 +2,38 @@
 #Install the package
 #install.packages(“e1071”)
 #Loading the library
+library(caret)
 library(e1071)
+library(DMwR)
 
 #Se cargan datos con la última columna modifcada (1 -> YES, -1 -> NO)
-BreastCancer_df = read.csv("nci60_binary_class_training_set.csv")
+datos = read.csv("nci60_binary_class_training_set.csv")
+#View(datos)
+str(datos) # busco columnas que puedan crear conflicto(que no son numero) con rowMeans()
+datos$Labels=as.factor(datos$Labels)
+# se quitan variables que sobran
+datos$ensembl_gene_id <- NULL
+datos$gene_biotype <- NULL
+datos$X <- NULL
+datos$entrezgene <- row.names(datos$entrezgene)
+datos$entrezgene <- NULL
+datos$chromosome_name <- NULL
+datos$LC.NCI.H23 <-NULL
+levels(datos$Labels) <- c("non_driver", "driver")
 
-final_test = read.csv("nci60_non_coding_test_set.csv")
+# se hace un shuffle de los elementos
+set.seed(156) 
+rand <- sample(nrow(datos))
+rand
+datos<-datos[rand, ]
+datos <- na.omit(datos) # se omiten filas con NA
+str(datos)
+#log2?
+datos[, 1:59] <- log(datos[1:59], 2)
 
-BreastCancer_df$LC.NCI.H23[is.na(BreastCancer_df$LC.NCI.H23)] <- 0 # lleno de 0's columna conflictiva
-
-BreastCancer_df$Labels=as.factor(BreastCancer_df$Labels)
-
-BreastCancer_df$ensembl_gene_id <- NULL
-BreastCancer_df$gene_biotype <- NULL
-BreastCancer_df$chromosome_name <- NULL
-
-
-
+#============================================
+# PARTICIONAMIENTO
+#============================================
 # setting the seed for reproducible results
 set.seed(420) 
 # separate the partitions between training and test set
@@ -27,16 +42,45 @@ trainIndex <- createDataPartition(datos$Labels,
                                   list = FALSE, 
                                   times = 1)
 
-training_set <- BreastCancer_df[ trainIndex, ]
-test_set <- BreastCancer_df[ -trainIndex, ]
+training_set <- datos[ trainIndex, ]
+test_set <- datos[ -trainIndex, ]
+#BALANCEO
+#============================================
+# Balanceo SMOTE, OVER 100%
+#============================================
+smoted_data <- SMOTE(Labels~., training_set, perc.over=100)
 
-Naive_Bayes_Model=naiveBayes(Labels~.,data=training_set)
+training_set <- smoted_data
+
+Naive_Bayes_Model=naiveBayes(Labels ~ LC.NCI.H522 + BR.MCF7 + LE.HL.60.TB. + CO.KM12 + RE.TK.10+
+                               LE.CCRF.CEM + OV.SK.OV.3 + CNS.SF.268 + RE.CAKI.1 + LC.NCI.H460 + OV.OVCAR.4
+                             + OV.IGROV1 + CO.HCT.15 + ME.UACC.62 + CO.SW.620 + RE.SN12C + RE.786.0 + OV.OVCAR.3+
+                               LE.K.562 + BR.BT.549 + RE.UO.31 + ME.M14 + OV.NCI.ADR.RES + ME.SK.MEL.2 + ME.MALME.3M
+                             + ME.SK.MEL.28 + BR.MDA.MB.231 + ME.MDA.N + LC.HOP.92 + BR.HS.578T + RE.ACHN + OV.OVCAR.8+
+                               RE.A498 + CNS.SNB.19 + CO.HT29 + ME.SK.MEL.5,
+                             data=training_set)
 
 summary(Naive_Bayes_Model)
-NB_Predictions=predict(Naive_Bayes_Model,test_set)
-summary(NB_Predictions)
-#Confusion matrix to 
-table(NB_Predictions,test_set$Labels)
-NB_Predictions_final=predict(Naive_Bayes_Model,final_test)
-summary(NB_Predictions_final)
+NB_Predictions=predict(Naive_Bayes_Model,training_set)
+confusionMatrix(NB_Predictions,training_set$Labels)
+
+NB_Predictions_test=predict(Naive_Bayes_Model,test_set)
+confusionMatrix(NB_Predictions_test,test_set$Labels)
+
+
+
+#NB, probando modelo con datos_testset sin clasificar
+#Predict Output 
+datos_testset = read.csv("nci60_non_coding_test_set.csv")
+datos_testset$ensembl_gene_id <- NULL
+datos_testset$gene_biotype <- NULL
+datos_testset$X <- NULL
+datos_testset$entrezgene <- row.names(datos_testset$entrezgene)
+datos_testset$entrezgene <- NULL
+datos_testset$chromosome_name <- NULL
+datos_testset$LC.NCI.H23 <-NULL
+datos_testset$hgnc_id<-NULL
+datos_testset <- na.omit(datos_testset)
+datos_testset <- log(datos_testset, 2)
+summary(predict(Naive_Bayes_Model , newdata = datos_testset, interval = 'prediction'))
 
--- svm.R
+++ svm.R
@@ -1,41 +1,48 @@
 library(ggplot2)
+library(ggtheme)
 library(e1071)
 library(stringr)
 library(rpart)
 library(rattle)
 library(caret)
 library(plyr)
-
-#
+library(Boruta)
+library(UBL)
+library(Metrics)
+library(dplyr)
+library(mlbench)
+library(DMwR)
+#============================================
 #PREPROCESAMIENTO DE DATOS
-#
-
+#============================================
 #cargo set de datos
-datos <- read.csv("nci60_binary_class_training_set.csv")
-datos_testset <- read.csv("nci60_non_coding_test_set.csv")
+datos <- read.csv("C:/Users/oscar/Documents/Aprendizaje Automatico CI2600/ultima entrega/nci60_binary_class_training_set.csv")
 #View(datos)
-
 str(datos) # busco columnas que puedan crear conflicto(que no son numero) con rowMeans()
-
-datos$LC.NCI.H23[is.na(datos$LC.NCI.H23)] <- 0 # lleno de 0's columna conflictiva
-
-
 datos$Labels=as.factor(datos$Labels)
-
+# se quitan variables que sobran
 datos$ensembl_gene_id <- NULL
 datos$gene_biotype <- NULL
-#datos$chromosome_name <- NULL
+datos$X <- NULL
+datos$entrezgene <- row.names(datos$entrezgene)
+datos$entrezgene <- NULL
+datos$chromosome_name <- NULL
+datos$LC.NCI.H23 <-NULL
+levels(datos$Labels) <- c("non_driver", "driver")
 
-datos_testset$ensembl_gene_id <- NULL
-datos_testset$gene_biotype <- NULL
-#datos_testset$chromosome_name <- NULL
-#datos_testset$hgnc_id <- NULL
-datos_testset$LC.NCI.H23[is.na(datos_testset$LC.NCI.H23)] <- 0 # lleno de 0's columna conflictiva
-
+# se hace un shuffle de los elementos
+set.seed(156) 
+rand <- sample(nrow(datos))
+rand
+datos<-datos[rand, ]
 datos <- na.omit(datos) # se omiten filas con NA
 str(datos)
+#log2?
+datos[, 1:59] <- log(datos[1:59], 2)
 
-
+#============================================
+# PARTICIONAMIENTO
+#============================================
 # setting the seed for reproducible results
 set.seed(420) 
 # separate the partitions between training and test set
@@ -46,86 +53,162 @@ trainIndex <- createDataPartition(datos$Labels,
 
 training_set <- datos[ trainIndex, ]
 test_set <- datos[ -trainIndex, ]
+#BALANCEO
+#============================================
+# Balanceo SMOTE, OVER 100%
+#============================================
+smoted_data <- SMOTE(Labels~., training_set, perc.over=100)
 
-#
-# FIN DE PREPROCESAMIENTO
-#
+training_set <- smoted_data
+
+#FEATURE SELECTION
+#============================================
+#RFE (~30 minutos) https://machinelearningmastery.com/feature-selection-with-the-caret-r-package/
+#============================================ 
+# ensure the results are repeatable
+set.seed(7)
+
+# define the control using a random forest selection function
+control <- rfeControl(functions=rfFuncs, method="cv", number=10)
+# run the RFE algorithm
+results <- rfe(datos[,1:59], datos[,60], sizes=c(1:59), rfeControl=control)
+# summarize the results
+print(results)
+# list the chosen features
+predictors(results)
+# plot the results
+plot(results, type=c("g", "o"))
+#=============================================
+# Feature selection lvq
+#=============================================
+control <- trainControl(method="repeatedcv", number=10, repeats=3)
+# train the model
+model <- train(Labels~., data=datos, method="lvq", preProcess="scale", trControl=control)
+# estimate variable importance
+importance <- varImp(model, scale=FALSE)
+# summarize importance
+print(importance)
+# plot importance
+plot(importance)
+#============================================
+#highly correlated https://machinelearningmastery.com/feature-selection-with-the-caret-r-package/
+#============================================ 
+# ensure the results are repeatable
+set.seed(7)
+# calculate correlation matrix
+correlationMatrix <- cor(datos[,1:59])
+# summarize the correlation matrix
+print(correlationMatrix)
+# find attributes that are highly corrected (ideally >0.75)
+highlyCorrelated <- findCorrelation(correlationMatrix, cutoff=0.5)
+# print indexes of highly correlated attributes
+print(highlyCorrelated)
 
 
-#CASO 1 SVM del profe
-# create svm model
-
-svm_model <- svm(Labels~., data=training_set, type='C-classification',kernel='radial')
-
-summary(svm_model)
-# prediccion sobre training set
-pred_train <-predict(svm_model,training_set)
-
-mean(pred_train==training_set$Labels)
-
-conf.mat.svm.train <- confusionMatrix(pred_train,training_set$Labels)
-
-conf.mat.svm.train
-
-# prediccion sobre test set
-pred_test <-predict(svm_model,test_set)
-summary(pred_test)
-conf.mat.svm.test <- confusionMatrix(pred_test, test_set$Labels)
-
-conf.mat.svm.test
-
-# percentage of testset predicted correctly by svm
-mean(pred_test==test_set$Labels)
-#
-
-#TUNE(grid search), dura bastante
-
-svm.tune.test <- tune.svm(Labels~.,data = datos, gamma=c(0.001, 0.01, 0.1, 1, 5, 10, 20, 50, 100, 150, 200), cost = (1:2))
-
-summary(svm.tune.test)
-plot(svm.tune.test)
-svm.tune.test$best.parameters
 
 
 
 
 
-# CASO 2
+
+
+
+
+#modelos:
+
+
+
 #svm de internet, https://dataaspirant.com/2017/01/19/support-vector-machine-classifier-implementation-r-caret-package/
+#=========================================
+#SVM RADIAL
+#=========================================
 trctrl <- trainControl(method = "repeatedcv", number = 10, repeats = 3)
 
 set.seed(3233)
-svm_Radial <- train(Labels ~., data = training_set, method = "svmRadial",
-                      trControl=trctrl,
-                      preProcess = c("center", "scale"),
-                      tuneLength = 10)
+svm_Radial <- train(Labels ~ LC.NCI.H522 + BR.MCF7 + LE.HL.60.TB. + CO.KM12 + RE.TK.10+
+                      LE.CCRF.CEM + OV.SK.OV.3 + CNS.SF.268 + RE.CAKI.1 + LC.NCI.H460 + OV.OVCAR.4
+                    + OV.IGROV1 + CO.HCT.15 + ME.UACC.62 + CO.SW.620 + RE.SN12C + RE.786.0 + OV.OVCAR.3+
+                      LE.K.562 + BR.BT.549 + RE.UO.31 + ME.M14 + OV.NCI.ADR.RES + ME.SK.MEL.2 + ME.MALME.3M
+                    + ME.SK.MEL.28 + BR.MDA.MB.231 + ME.MDA.N + LC.HOP.92 + BR.HS.578T + RE.ACHN + OV.OVCAR.8+
+                      RE.A498 + CNS.SNB.19 + CO.HT29 + ME.SK.MEL.5, 
+                    data = training_set, method = "svmRadial",
+                    trControl=trctrl,
+                    preProcess = c("center", "scale"),
+                    tuneLength = 10)
 svm_Radial
 plot(svm_Radial)
 
-test_pred_Radial <- predict(svm_Radial, newdata = test_set)
-confusionMatrix(test_pred_Radial, test_set$Labels)
+# RESULTADO SET DE ENTRENAMIENTO
+test_pred_Radial1 <- predict(svm_Radial, newdata = training_set)
+confusionMatrix(test_pred_Radial1, training_set$Labels)
+# RESULTADO SET DE PRUEBA
+test_pred_Radial2 <- predict(svm_Radial, newdata = test_set)
+confusionMatrix(test_pred_Radial2, test_set$Labels)
 
 
 
 # grid search, dura bastante
 grid_radial <- expand.grid(sigma = c(0,0.01, 0.02, 0.025, 0.03, 0.04,0.05, 0.06, 0.07,0.08, 0.09, 0.1, 0.25, 0.5, 0.75,0.9),
                            C = c(0,0.01, 0.05, 0.1, 0.25, 0.5, 0.75,
-                                   1, 1.5, 2,5))
+                                 1, 1.5, 2,5))
 set.seed(3233)
 
-svm_Radial_Grid <- train(Labels ~., data = training_set, method = "svmRadial",
-                           trControl=trctrl,
-                           preProcess = c("center", "scale"),
-                           tuneGrid = grid_radial,
-                           tuneLength = 10)
+svm_Radial_Grid <- train(Labels ~LC.NCI.H522 + BR.MCF7 + LE.HL.60.TB. + CO.KM12 + RE.TK.10+
+                           LE.CCRF.CEM + OV.SK.OV.3 + CNS.SF.268 + RE.CAKI.1 + LC.NCI.H460 + OV.OVCAR.4
+                         + OV.IGROV1 + CO.HCT.15 + ME.UACC.62 + CO.SW.620 + RE.SN12C + RE.786.0 + OV.OVCAR.3+
+                           LE.K.562 + BR.BT.549 + RE.UO.31 + ME.M14 + OV.NCI.ADR.RES + ME.SK.MEL.2 + ME.MALME.3M
+                         + ME.SK.MEL.28 + BR.MDA.MB.231 + ME.MDA.N + LC.HOP.92 + BR.HS.578T + RE.ACHN + OV.OVCAR.8+
+                           RE.A498 + CNS.SNB.19 + CO.HT29 + ME.SK.MEL.5,
+                         data = training_set, method = "svmRadial",
+                         trControl=trctrl,
+                         preProcess = c("center", "scale"),
+                         tuneGrid = grid_radial,
+                         metric = "Kappa",
+                         tuneLength = 10)
 
 svm_Radial_Grid
 
-test_pred_Radial_Grid <- predict(svm_Radial_Grid, newdata = test_set)
- 
-confusionMatrix(test_pred_Radial_Grid, test_set$Labels )
 
 
+# RESULTADO SET DE ENTRENAMIENTO
+test_pred_Radial_Grid2 <- predict(svm_Radial_Grid, newdata = training_set)
+confusionMatrix(test_pred_Radial_Grid2, training_set$Labels )
+# RESULTADO SET DE PRUEBA
+test_pred_Radial_Grid1 <- predict(svm_Radial_Grid, newdata = test_set)
+confusionMatrix(test_pred_Radial_Grid1, test_set$Labels )
+
+
+
+
+#=========================================
+#SVM LINEAR
+#=========================================
+trctrl <- trainControl(method = "repeatedcv", number = 10, repeats = 3)
+
+set.seed(3233)
+svm_Radial <- train(Labels ~ LC.NCI.H522 + BR.MCF7 + LE.HL.60.TB. + CO.KM12 + RE.TK.10+
+                      LE.CCRF.CEM + OV.SK.OV.3 + CNS.SF.268 + RE.CAKI.1 + LC.NCI.H460 + OV.OVCAR.4
+                    + OV.IGROV1 + CO.HCT.15 + ME.UACC.62 + CO.SW.620 + RE.SN12C + RE.786.0 + OV.OVCAR.3+
+                      LE.K.562 + BR.BT.549 + RE.UO.31 + ME.M14 + OV.NCI.ADR.RES + ME.SK.MEL.2 + ME.MALME.3M
+                    + ME.SK.MEL.28 + BR.MDA.MB.231 + ME.MDA.N + LC.HOP.92 + BR.HS.578T + RE.ACHN + OV.OVCAR.8+
+                      RE.A498 + CNS.SNB.19 + CO.HT29 + ME.SK.MEL.5, 
+                    data = training_set, method = "svmLinear",
+                    trControl=trctrl,
+                    preProcess = c("center", "scale"),
+                    tuneLength = 10)
+svm_Radial
+
+# RESULTADO SET DE ENTRENAMIENTO
+test_pred_Radial1 <- predict(svm_Radial, newdata = training_set)
+confusionMatrix(test_pred_Radial1, training_set$Labels)
+# RESULTADO SET DE PRUEBA
+test_pred_Radial2 <- predict(svm_Radial, newdata = test_set)
+confusionMatrix(test_pred_Radial2, test_set$Labels)
+
+
+
+
+#=========================================
 
 #SVM, classification, probando modelo con datos sin clasificar
 
